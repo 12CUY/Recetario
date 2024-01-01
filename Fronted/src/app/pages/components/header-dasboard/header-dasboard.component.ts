cf31802429f43953cc5a62dcf925539cc1fafc00
12CUY <wiliampuntomorales45@gmail.com>
2023-12-31 21:47:25 -0500

    carpetas nuevas

---
+++ Fronted/src/app/pages/components/header-dasboard/header-dasboard.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header-dasboard',
+  standalone: true,
+  imports: [],
+  templateUrl: './header-dasboard.component.html',
+  styleUrl: './header-dasboard.component.css'
+})
+export class HeaderDasboardComponent {
+
+}
